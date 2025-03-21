a673249f685dea07e89fab4fb8be16112e5f703c
Made <2020-051183@intecap.edu.gt>
2025-03-20 20:47:43 -0600

    Se ingresa proyecto inicial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Inventario");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese Nombre del Producto Nuevo: ");
+        String productoNuevo = scanner.nextLine();
+
+        System.out.println("Ingrese Precio del Producto");
+        int precioProducto = scanner.nextInt();
+
+        scanner.close();
+    }
+}
